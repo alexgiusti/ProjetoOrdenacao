1cd51e16f4e191790cc0b5130627cf3924b5b31e
Fabio Henrique <fabinho_mazetti@hotmail.com>
2021-10-08 22:53:44 -0300

    Mudando o nome do meu projeto para melhor organização

--- ProjetoVersionamentoCodigo.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main (){
-    int vetor[100], aux;
-
-    for (int i = 0; i < 100; i++){
-        printf("Numero posicao %d no vetor", i);
-        scanf("%d", &vetor[i]);
-    }
-}
