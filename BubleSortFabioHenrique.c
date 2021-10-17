7b8686623ecbdb8d7a40db97dd9ee0b9eab443c5
Fabio Henrique <fabinho_mazetti@hotmail.com>
2021-10-17 14:40:33 -0300

    inserindo função Random

--- BubleSortFabioHenrique.c
+++ BubleSortFabioHenrique.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
-#include <stdlib.h>   
+#include <stdlib.h> 
+#include <time.h>  
 int main (void){
 
 
     int qnt_elementos, aux, vetor;
-    int *n;
+    int *n; // MEU VETOR 
 
     printf("Digite a quantidade de elementos do seu vetor \n");
     scanf("%d", &qnt_elementos);
     n =  (int *)(malloc (qnt_elementos * sizeof(int)));
     
-
+    srand(time(NULL));
     for (int i = 0; i < qnt_elementos; i++){
-        printf("Numero posicao %d no vetor \n ", i);
-        scanf("%d", &n[i]);
+        n[i] = rand();
     }
     for (int x = 0; x < qnt_elementos; x++){
         for(int y = x; y < qnt_elementos; y++){
@@ -27,5 +27,7 @@ int main (void){
     for ( int i = 0; i < qnt_elementos; i++){
         printf("numero %d eh: %d \n", i, n[i]);
     }
+    
+    free(n);
     
 }
