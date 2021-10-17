c04cacf46ab89218e5cddff0de4864c0bdd99385
Fabio Henrique <fabinho_mazetti@hotmail.com>
2021-10-17 15:42:21 -0300

    Inserindo a escolha do range do numero ao usuario

--- BubleSortFabioHenrique.c
+++ BubleSortFabioHenrique.c
@@ -4,30 +4,31 @@
 int main (void){
 
 
-    int qnt_elementos, aux, vetor;
-    int *n; // MEU VETOR 
+    int qnt_elementos, aux, range_numero;
+    int *vetor; 
 
     printf("Digite a quantidade de elementos do seu vetor \n");
     scanf("%d", &qnt_elementos);
-    n =  (int *)(malloc (qnt_elementos * sizeof(int)));
-    
+    vetor =  (int *)(malloc (qnt_elementos * sizeof(int)));
     srand(time(NULL));
+    printf("Informe ate que numero sera gerado os numeros aleatorios \n");
+    scanf("%d", &range_numero);
     for (int i = 0; i < qnt_elementos; i++){
-        n[i] = rand();
+       vetor[i] = rand() % range_numero;
     }
     for (int x = 0; x < qnt_elementos; x++){
         for(int y = x; y < qnt_elementos; y++){
-            if(n[x] > n[y]){
-                aux = n[x];
-                n[x] = n[y];
-                n[y] = aux;
+            if(vetor[x] > vetor[y]){
+                aux = vetor[x];
+                vetor[x] = vetor[y];
+                vetor[y] = aux;
             }
         }
     }
     for ( int i = 0; i < qnt_elementos; i++){
-        printf("numero %d eh: %d \n", i, n[i]);
+        printf("numero %d eh: %d \n", i, vetor[i]);
     }
     
-    free(n);
+    free(vetor);
     
 }
