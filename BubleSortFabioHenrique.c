a1a02211a32a8070916864ae02dd538e9bfc9b9b
Fabio Henrique <fabinho_mazetti@hotmail.com>
2021-10-17 11:45:57 -0300

    buble sort criado e testada com sucesso

--- BubleSortFabioHenrique.c
+++ BubleSortFabioHenrique.c
@@ -1,9 +1,22 @@
 #include <stdio.h>
 int main (){
-    int vetor[100], aux;
+    int vetor[5], aux;
 
-    for (int i = 0; i < 100; i++){
-        printf("Numero posicao %d no vetor", i);
+    for (int i = 0; i < 5; i++){
+        printf("Numero posicao %d no vetor \n ", i);
         scanf("%d", &vetor[i]);
+    }
+    for (int x = 0; x < 5; x++){
+        for(int y = x; y < 5; y++){
+            if(vetor[x] > vetor[y]){
+                aux = vetor[x];
+                vetor[x] = vetor[y];
+                vetor[y] = aux;
+            }
+        }
+    }
+    if()
+    for ( int i = 0; i < 5; i++){
+        printf("numero %d eh: %d \n", i, vetor[i]);
     }
 }
