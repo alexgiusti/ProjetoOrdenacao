299b905b2356cb301e486fd8ad7cb99fdf3bb90c
Alexsander Cesar Giusti <37516606+alexgiusti@users.noreply.github.com>
2021-10-18 10:24:37 -0300

    Alterado o algoritmo main
    O algoritmo foi alterado e foi adicionado o switch case de seleção do método de organização para abrigar as funções das bibliotecas.

--- main.c
+++ main.c
@@ -1,22 +1,49 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <conio.h>
 
 int main()
 {
-    int tam=0;
+    int tam;
+    int i;
+    int metodo;
 
-    printf("Bem vindo ao sistema de Organização de Vetores :)");
+    printf("Bem vindo ao sistema de Organizacao de Vetores :)");
     printf("\nPor favor, insira o tamanho do vetor que deseja ordenar:");
-    scanf("%d",&tam);
-    printf("\n\nO tamanho do vetor selecionado foi de ",tam," casas;");
+    scanf("%d", &tam);
+    printf("\n\nO tamanho do vetor selecionado foi de %d casas",tam);
 
     int vetor[tam];
-
-    for (int i = 0; i < tam; i++)
+    printf("\n\nVetor criado:");
+    for(i = 0; i < tam; i++)
     {
         vetor[i]= rand() % tam*2;
-        printf(vetor[i]);
+        printf(" %d",vetor[i]);
     } 
+	printf("Selecione o metodo de ordenacao: \n\n1:Bubble Sort\n2:Selection Sort\n3:Insertion Sort\n4:Shell Sort");
+	scanf("%d", &metodo)
 	
-	
+	switch(metodo)
+	{
+		case 1 :
+			printf("\n\nMetodo Bubble Sort Selecionado");
+			break;
+
+		case 2 :
+			printf("\n\nMetodo Selection Sort Selecionado");
+			break;
+
+		case 3 :
+			printf("\n\nMetodo Insertion Sort Selecionado");
+			break;
+
+		case 4 :
+			printf("\n\nMetodo Shell Sort Selecionado");
+			break;
+
+		default :
+			printf("\n\nMetodo Invalido");
+			break;												
+	}
+	getch();
 }
