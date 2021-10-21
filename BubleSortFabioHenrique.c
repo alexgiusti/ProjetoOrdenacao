0f412445e2df0bfa5ddceb09426078fb2f3b3c44
Fabio Henrique <fabinho_mazetti@hotmail.com>
2021-10-21 20:11:23 -0300

    Inserindo biblioteca do Buble Sort

--- BubleSortFabioHenrique.c
+++ BubleSortFabioHenrique.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h> 
-#include <time.h>  
+#include <time.h> 
+#include "BubleSortFabioHenrique.h" 
 
 int BubleSort(int qnt_elementos, int range_numero);
 
---
+++ BubleSortFabioHenrique.h
@@ -0,0 +1,12 @@
+#ifndef BubleSortFabioHenrique_H
+#define BubleSortFabioHenrique_H
+
+
+//int qnt_elementos, range_numero;
+int BubleSort(int qnt_elementos, int range_numero);
+
+ 
+
+
+
+#endif
