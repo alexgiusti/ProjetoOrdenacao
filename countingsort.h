552d1fb74073542d5dbd1ea33b07847433d44eba
vfmattos <92189050+vfmattos@users.noreply.github.com>
2021-10-29 17:23:05 -0300

    Update and rename countingSort.h to countingsort.h

--- countingsort.h
+++ countingsort.h
@@ -1,5 +1,5 @@
-#ifndef COUNTINGSORT_H
-#define COUNTINGSORT_H
+#ifndef COUNTINGSORT_H_INCLUDED
+#define COUNTINGSORT_H_INCLUDED
 
 void countingSort(int nums[], int tam);
 void imprimeLista(int nums[], int tam);
