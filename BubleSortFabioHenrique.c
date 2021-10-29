cc6d506f69c3094c4baa470784b64e7278d95266
Fabio Henrique <fabinho_mazetti@hotmail.com>
2021-10-29 18:13:09 -0300

    Finalizando biblioteca com função

--- BubleSortFabioHenrique.c
+++ BubleSortFabioHenrique.c
@@ -11,44 +11,5 @@ int main (void){
    
     }
 
-    int BubleSort(int qnt_elementos, int range_numero){
-    int aux;
-    int *vetor; 
-
-   /* printf("Digite a quantidade de elementos do seu vetor \n");
-      scanf("%d", &qnt_elementos); 
-      Caso o usuario queira interagir com a interface
-      */
-
-    vetor =  (int *)(malloc (qnt_elementos * sizeof(int)));
-    srand(time(NULL));
-
-    /*printf("Informe ate que numero sera gerado os numeros aleatorios \n");
-      scanf("%d", &range_numero); 
-      Caso o usuario queira interagir com a interface */
-      if(qnt_elementos == 0 || range_numero == 0)
-          printf("Os numeros dos argumentos sao invalidos, tente outros \n");
-      
-      else 
-          printf("Numeros validos para ordenacao, Ordenando \n");
-      
-
-    for (int i = 0; i < qnt_elementos; i++){
-       vetor[i] = rand() % range_numero;
-    }
-    for (int x = 0; x < qnt_elementos; x++){
-        for(int y = x; y < qnt_elementos; y++){
-            if(vetor[x] > vetor[y]){
-                aux = vetor[x];
-                vetor[x] = vetor[y];
-                vetor[y] = aux;
-            }
-        }
-    }
-    for ( int i = 0; i < qnt_elementos; i++){
-        printf("numero %d eh: %d \n", i, vetor[i]);
-    }
     
-    free(vetor);
-    }
                 
