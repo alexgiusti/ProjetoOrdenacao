fe553b2e7752a3ab0180a00a1e56df08fabee5ed
leonardorici <88636808+leonardorici@users.noreply.github.com>
2021-10-29 19:09:33 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "shellsort.h"
+#include "countingsort.h"
+#include "InsertionSort.h"
+#include "selectionsort.h"
+
+
+
+void mostraVetor(int vetor[], int tam)
+{
+	int i;
+    for (i=0; i<tam; i++)
+       printf("%d ",vetor[i]);
+}
+
+
+int main()
+{
+    int tam;
+    int i;
+    int metodo;
+
+    printf("Bem vindo ao sistema de Organizacao de Vetores :)");
+    printf("\nPor favor, insira o tamanho do vetor que deseja ordenar: ");
+    scanf("%d", &tam);
+    printf("\n\nO tamanho do vetor selecionado foi de %d casas",tam);
+
+    int vetor[tam];
+    printf("\n\nVetor criado: ");
+    for(i = 0; i < tam; i++)
+    {
+        vetor[i]= rand() % tam*2;
+    } 
+
+	mostraVetor(vetor, tam);
+	
+	printf("\n\nSelecione o metodo de ordenacao: \n\n1:Bubble Sort\n2:Selection Sort\n3:Insertion Sort\n4:Shell Sort\n5:Counting Sort\n\n");
+	scanf("%d", &metodo);
+	
+	switch(metodo)
+	{
+		case 1 :
+			printf("\n\nMetodo Bubble Sort Selecionado");
+			BubleSort(vetor, tam);
+			mostraVetor(vetor, tam);
+			break;
+
+		case 2 :
+			printf("\n\nMetodo Selection Sort Selecionado");
+            selectionsort(vetor, tam);
+			break;
+
+		case 3 :
+			printf("\n\nMetodo Insertion Sort Selecionado");
+			InsertionSort( vetor,  tam);
+
+			break;
+
+		case 4 :
+			printf("\n\nMetodo Shell Sort Selecionado\n\nVetor Ordenado: ");
+			shellSort(vetor, tam);
+			mostraVetor(vetor, tam);
+			break;
+			
+		case 5:
+			printf("\n\nMetodo Counting Sort Selecionado\n\nVetor Ordenado: ");
+			countingSort(vetor, tam);
+			mostraVetor(vetor, tam);
+			break;
+
+		default :
+			printf("\n\nMetodo Invalido");
+			break;												
+	}
+}
