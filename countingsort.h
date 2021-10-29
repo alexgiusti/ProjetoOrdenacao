0ca3f30a34099d07f9ccf0bfe59ad79f04cb9645
vfmattos <92189050+vfmattos@users.noreply.github.com>
2021-10-29 18:06:04 -0300

    Update countingsort.h

--- countingsort.h
+++ countingsort.h
@@ -1,7 +1,40 @@
 #ifndef COUNTINGSORT_H_INCLUDED
 #define COUNTINGSORT_H_INCLUDED
 
-void countingSort(int nums[], int tam);
-void imprimeLista(int nums[], int tam);
+void countingSort(int nums[], int tam) {
+  int output[10];
+  int i;
+
+
+  int max = nums[0];
+  for (i = 1; i < tam; i++) {
+    if (nums[i] > max)
+      max = nums[i];
+  }
+
+  int count[10];
+
+  for (i = 0; i <= max; ++i) {
+    count[i] = 0;
+  }
+
+  for (i = 0; i < tam; i++) {
+    count[nums[i]]++;
+  }
+
+  for (i = 1; i <= max; i++) {
+    count[i] += count[i - 1];
+  }
+
+ 
+  for (i = tam - 1; i >= 0; i--) {
+    output[count[nums[i]] - 1] = nums[i];
+    count[nums[i]]--;
+  }
+
+  for (i = 0; i < tam; i++) {
+    nums[i] = output[i];
+  }
+}
 
 #endif
