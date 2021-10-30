d18e0c3bfe2ea759a50c00cd4aed1e1208c0ee1c
vfmattos <92189050+vfmattos@users.noreply.github.com>
2021-10-30 10:47:32 -0300

    Update SelectionSort.h

--- SelectionSort.h
+++ SelectionSort.h
@@ -6,7 +6,7 @@ int selectionSort (int vetor[], int tam){
 
   for(i=0; i<tam-1; i++){
       for(k=i+1; k<tam; k++){
-          if(vetor[k] < vetor [i]{ 
+          if(vetor[k] < vetor [i]){ 
             t = vetor[i];
             vetor[i] = vetor[k];
             vetor[k] = t;
@@ -14,13 +14,14 @@ int selectionSort (int vetor[], int tam){
       }  
   }
   
-  printf ("\n Os números ordenados: \n");
+  printf("\n Os nÃºmeros ordenados: \n");
   
-  for(k=0; k<tam; k++){
-      printf("%i\n", v[k]);
+  for(k = 0; k < tam; k++){
+      printf("%i\n", vetor[k]);
    
  return 0;
   
  }
+}
 
 #endif
